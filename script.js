8e50a1a0150c8139740981adab26ac50509e5e06
pablo <hortonwhy@protonmail.com>
2021-03-08 23:58:58 -0600

    added some basic functionality for js stuff

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-alert("yo");
+var showingDetail = false;
+
+function showDetail (region) {
+  createBox(region, region.x *1.2, region.y * 1.2)
+}
+
+function createBox (node, xX, yY) {
+  if (showingDetail) {
+    toDel = document.getElementById("details")
+    if (toDel != null) {
+      toDel.remove()
+      showingDetail = false;
+    }
+  } else {
+    var mapNode = node.parentNode;
+    var boxDiv = document.createElement("div"); 
+    var box = document.createElement("p");
+    boxDiv.setAttribute("id", "details");
+    mapNode.appendChild(boxDiv);
+    boxDiv.appendChild(box);
+
+    // set its position
+    boxDiv.style.left = xX+"px";
+    boxDiv.style.top = yY+"px";
+
+    console.log(boxDiv);
+    box.innerHTML = "<strong>HEYYY!!!!!</strong>";
+    showingDetail = true; // html is added. therefore if clicked again it should delete it
+  }
+ }
+
