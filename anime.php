23e133cb88781a6e3ff5d5b28ba152b5f92e622c
flashandgr <chencatherine99@gmail.com>
2021-04-17 14:53:51 -0500

    Update anime.php

--- anime.php
+++ anime.php
@@ -1,7 +1,12 @@
 <?php
+session_start();
+
 if ($_POST["page"] == "confirm") {
 	confirmPage();
-} else {
+} elseif ($_POST["page"] == "confirmed") {
+	lastPage();
+}	
+else {
 	chooseAnime();
 }
 
@@ -60,7 +65,7 @@ PAGE1;
 }
 
 function confirmPage() {
-	$animeSelected = $_POST["anime"];
+	$_SESSION['chooseAnime'] = $_POST['anime'];
 print <<<PAGE2
 
 <html lang="en">
@@ -91,15 +96,24 @@ print <<<PAGE2
         <div id="description">
             <p>
             <div id="map">
-              <h3 class="align-center">Anime You Selected: </h3>
+	      <h3 class="align-center">Here is All the Anime You Selected. Please Confirm.</h3>
+<h4 class="align-center">If you missed any, you can select more from the table. Please reselect the ones listed as well.</h4>
 	      <form method='post' action="./anime.php">
 PAGE2;
+ $count = 0;
 foreach ($_POST['anime'] as $anime) {
-	#echo $anime . "<br>";
+	$animeSelected[] = $anime;
 	print("<li> $anime </li>");
+	print("<br>");
+	$count++;
 }
-
 print <<<PAGE2CONT
+
+              <table id="animeList" border=2>
+	      </table>
+		<input type="hidden" name="page" value="confirm" />
+		<input type="hidden" name="page" value="confirmed" /><br>
+		<input type="submit" value="Confirm" />
               </form>
               <br><br><br><br><br>
               <br><br><br><br><br>
@@ -121,4 +135,68 @@ print <<<PAGE2CONT
 </html>
 PAGE2CONT;
 }
+
+function lastPage() {
+	$temp = $_SESSION['chooseAnime'];
+print <<<PAGE2
+
+<html lang="en">
+
+<head>
+    <title>Dokidoki Travel</title>
+  <link rel="stylesheet" href="main.css">
+  <script src="script.js" defer></script>
+  <script src="AnimeList.js" defer></script>
+</head> 
+
+<body>
+<a href="index.html"><img id="logo" src="assets/images/logo.png" alt="logo pic" width="125px"></a>
+    <div id="navBar">
+        <table>
+            <tr>
+                <!--<th><img src="assets/images/logo.png" alt="logo pic" width="75px"></th>-->
+                <th><a href="index.html">ドキドキ Travel</a></th>
+                <th><a href="anime.html">Anime</a></th>
+                <th><a href="prefectures.html">Prefectures</a></th>
+                <th><a href="aboutUs.html">About Us</a></th>
+                <th><a href="contactUs.html">Contact Us</a></th>
+            </tr>
+        </table>
+    </div>
+
+    <div id="body">
+        <div id="description">
+            <p>
+            <div id="map">
+	      <h3 class="align-center"></h3>
+PAGE2;
+$count = 0;
+foreach ($temp as $anime) {
+	print("<li> $anime </li>");
+	print("<br>");
+	$count++;
+}
+print <<<LASTPAGE
+	<p><b> Amount Watched:</b> $count </p>	
+              <br><br><br><br><br>
+              <br><br><br><br><br>
+            </div>
+                <!-- Content -->
+            </p>
+            <p>
+                <!-- Content -->
+            </p>
+        </div>
+
+    </div>
+
+    <div id="footer">
+        <p>Copyright © 3/09/2021  Catherine Chen, Wyatt Horton, Rachel Kim, Jack Spicer</p>
+    </div>
+</body>
+
+</html>
+LASTPAGE;
+}
+
 ?>
