9a099f99148ed314794dd4f0b78d5e8c2e38585d
Rachel Kim <rachel@cs.utexas.edu>
2021-04-30 18:45:09 -0500

    Added a pop up message for email validation

--- verification.js
+++ verification.js
@@ -1,7 +1,11 @@
 function isEmail() {
 	var email = document.forms["newsletter"]["email"].value;
 	const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
+    if (!re.test(String(email).toLowerCase())){
+		alert("Please enter a valid email.");
+		return false;
+	}
+	return true;
 	// if (!email.match(alphanum)){
 	// 	alert("Please remove special characters.")
 	// 	return false;
